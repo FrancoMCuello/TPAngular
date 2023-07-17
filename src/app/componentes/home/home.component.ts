56b65f1fb930e4e456159c2d44addfdbfdda60b3
FrancoMCuello <franco.m.cuello@gmail.com>
2023-07-17 15:35:30 -0300

    Se agrego la funcion 'Filtro' practicando un poco mas NgFor y Ng if

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { CommonModule } from '@angular/common';
@@ -7,6 +7,10 @@ import { CartService } from '../../Servicios/cart.service';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-home',
@@ -14,12 +18,15 @@ import { MatDividerModule } from '@angular/material/divider';
   styleUrls: ['./home.component.css'],
   standalone: true,
   imports: [
+    FormsModule,
     MatGridListModule,
     NavbarComponent,
     CommonModule,
     MatButtonModule,
     MatDividerModule,
     MatIconModule,
+    MatToolbarModule, 
+    MatButtonToggleModule,
    ]
 })
 
@@ -32,35 +39,68 @@ export class HomeComponent {
       name: 'Mesa',
       price: 15000,
       count:0,
+      categoria: 'Muebles',
     },
     {
       id: 2,
       name: 'Heladera',
       price: 200000,
       count:0,
+      categoria: 'Electrodomesticos',
     },
     {
       id:3,
       name: 'Silla',
       price: 4000,
       count: 0,
+      categoria: 'Muebles',
       
     }
   ];
 
+  oFilter: boolean = false;
+  filter: string = 'all';
+
+  categoriaSeleccionada: string = '';
 
   constructor(private cartService: CartService){
   };
 
+  //Mostrar Filtro
+  showFilter():void{
+    this.oFilter = true;
+    }
+
+  //Agregar y Quitar del Carrito
   addToCart(product: Product){
     this.cartService.addCart(product);
   }
-  
   
   deleteToCart(product: Product){
     this.cartService.deleteCart(product);
   }
 
+  //Aplicar filtro
+
+  selectAll(){
+    this.categoriaSeleccionada = '';
+  }
+
+  selectElect(){
+    this.categoriaSeleccionada = "Electrodomesticos";
+  }
+  selectMuebles(){
+    this.categoriaSeleccionada = "Muebles";
+  }
+
+  filtrarProductos() {
+    if (this.categoriaSeleccionada === '') {
+      return this.products;
+    } else {
+      return this.products.filter(product => product.categoria === this.categoriaSeleccionada);
+    }
+  }
+  
 }
 
 
--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -5,13 +5,20 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { CartService } from '../../Servicios/cart.service';
 import { CommonModule } from '@angular/common';
 
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, CommonModule],
+  imports: [
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    CommonModule,
+    FormsModule
+  ]
 })
 
 
@@ -23,11 +30,19 @@ constructor(private CartService: CartService){
 productCount: number = 0;
 cartPrice: number = 0;
 
+productElec: string = "Heladera";
+
 ngOnInit(): void{
+  //Metodo para ver los productos agregados y el precio del carrito
   this.CartService.cartSubject.subscribe( cart => {
-    this.productCount = cart.products.length;
-    this.cartPrice = cart.price;
+  this.productCount = cart.products.length;
+  this.cartPrice = cart.price;
   });
-}
+
+};
+
+
 
 }
+
+
